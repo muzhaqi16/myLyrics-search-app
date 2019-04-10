dc2877fa5b3a767d6b678c8144d1815f27314352
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2019-04-10 14:02:07 +0200

    fix yt-player display

--- app.js
+++ app.js
@@ -90,7 +90,9 @@ function displayLyrics(data,id,url){
   $('#songInfo').empty();
   $('#songInfo').append(`<li class="lyrics-info">
     <h2>${data.items[0].snippet.title}</h2>
+    <div class="yt-player">
     <iframe width="560" height="315" src="https://www.youtube.com/embed/${data.items[0].id.videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <div>
     <p id="lyrics_text"></p>
     <a href="${url}" target="_blank">See Full Lyrics</a></li>
   `);
