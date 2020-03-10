b69bdd19cd7d2e0ce2b6afd9dc59653938cb166a
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2020-03-10 19:11:55 -0400

    fix the spinner not loading

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,59 +2,60 @@
 
 const musixMatchApiKey = '18b2db314b4ef2be9c11ec52568a51e2';
 const musixMatchTrackUrl = 'https://api.musixmatch.com/ws/1.1/track.search';
-const musixMatchLyricsUrl='https://api.musixmatch.com/ws/1.1/track.lyrics.get'
+const musixMatchLyricsUrl = 'https://api.musixmatch.com/ws/1.1/track.lyrics.get'
 
-const youtubeApiKey = 'AIzaSyAwnrZEpsOnApIIpD6ytoPyozSxzFTihaU'; 
+const youtubeApiKey = 'AIzaSyAwnrZEpsOnApIIpD6ytoPyozSxzFTihaU';
 const youtubeSearchURL = 'https://www.googleapis.com/youtube/v3/search';
 
 //bypass the Access-Control-Allow-Origin error
 const bypassMusixMatchCors = 'https://cors-anywhere.herokuapp.com/';
 
 function formatQueryParams(params) {
-    const queryItems = Object.keys(params)
-        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-    return queryItems.join('&');
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
 }
 
-function searchForLyrics(query){
-    const params = {
-        q: query,
-        apikey: musixMatchApiKey,
-        s_track_rating:'desc',
-        format:"json",
-        f_has_lyrics:true,
-        page_size:9,
-      };
-    
-      const queryString = formatQueryParams(params);
-
-      let urlQuery = musixMatchTrackUrl +"?"+queryString;
-
-      fetch(bypassMusixMatchCors+urlQuery)
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          }
-          throw new Error(response);
-        })
-        .then(responseJson =>{
-          getSongInfo(responseJson.message.body.track_list)})
-        .catch(err => {
-          $('#js-error-message').text(`Something went wrong: ${err.message}`);
-        });
-}
-
-function getLyrics(trackId){
+function searchForLyrics(query) {
   const params = {
+    q: query,
     apikey: musixMatchApiKey,
-    track_id:trackId
+    s_track_rating: 'desc',
+    format: "json",
+    f_has_lyrics: true,
+    page_size: 9,
   };
 
   const queryString = formatQueryParams(params);
 
-  let urlQuery = musixMatchLyricsUrl +"?"+queryString;
+  let urlQuery = musixMatchTrackUrl + "?" + queryString;
 
-  fetch(bypassMusixMatchCors+urlQuery)
+  fetch(bypassMusixMatchCors + urlQuery)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response);
+    })
+    .then(responseJson => {
+      getSongInfo(responseJson.message.body.track_list)
+    })
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function getLyrics(trackId) {
+  const params = {
+    apikey: musixMatchApiKey,
+    track_id: trackId
+  };
+
+  const queryString = formatQueryParams(params);
+
+  let urlQuery = musixMatchLyricsUrl + "?" + queryString;
+
+  fetch(bypassMusixMatchCors + urlQuery)
     .then(response => {
       if (response.ok) {
         return response.json();
@@ -68,10 +69,10 @@ function getLyrics(trackId){
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
 }
-function getSongInfo(track_list){
-  if(track_list.length>0){
+function getSongInfo(track_list) {
+  if (track_list.length > 0) {
     $('#songInfo').empty();
-    track_list.forEach(trackObj=>{
+    track_list.forEach(trackObj => {
       $('#songInfo').append(`
       <li class="music-info">
         <h2 class="artist_name">${trackObj.track.artist_name}</h2>
@@ -82,13 +83,13 @@ function getSongInfo(track_list){
       </li>
       `);
     });
-  }else{
+  } else {
     $('#songInfo').empty();
-    $('.js-error-message').text('There were no matching results for your search query. Please check your spelling and try again' );
+    $('.js-error-message').text('There were no matching results for your search query. Please check your spelling and try again');
     $('.js-error-message').removeClass("hidden");
   }
 }
-function displayLyrics(data,id,url){
+function displayLyrics(data, id, url) {
   $('#songInfo').empty();
   $('#songInfo').append(`<li class="lyrics-info">
     <h2>${data.items[0].snippet.title}</h2>
@@ -100,14 +101,14 @@ function displayLyrics(data,id,url){
   `);
   getLyrics(id);
 }
-function getYoutubeVideo(query,id,url){
+function getYoutubeVideo(query, id, url) {
   const params = {
     key: youtubeApiKey,
     q: query,
     part: 'snippet',
-    maxResults:1,
+    maxResults: 1,
     type: 'video',
-    videoEmbeddable:"true"
+    videoEmbeddable: "true"
   };
   const queryString = formatQueryParams(params)
   const queryUrl = youtubeSearchURL + '?' + queryString;
@@ -119,34 +120,34 @@ function getYoutubeVideo(query,id,url){
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => displayLyrics(responseJson,id,url))
+    .then(responseJson => displayLyrics(responseJson, id, url))
     .catch(err => {
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
 
 }
-function handleGetYoutubeVideo(){
-  $('#songInfo').on('click','.getVideoButton',function(){
-    getYoutubeVideo($(this).siblings('.artist_name').text() + " " + $(this).siblings('.track_name').text(),$(this).siblings('.track_id').text(),$(this).siblings('.track_share_url').text());
+function handleGetYoutubeVideo() {
+  $('#songInfo').on('click', '.getVideoButton', function () {
+    getYoutubeVideo($(this).siblings('.artist_name').text() + " " + $(this).siblings('.track_name').text(), $(this).siblings('.track_id').text(), $(this).siblings('.track_share_url').text());
   });
 }
-function handleGetLyrics(){
-  $('#songInfo').on('click','.getLyricsButton',function(){
+function handleGetLyrics() {
+  $('#songInfo').on('click', '.getLyricsButton', function () {
     getLyrics($(this).siblings('.track_id').text());
   });
 }
-function handleSearch(){
-    $('#searchArea').on('submit',event=>{
-        event.preventDefault();
-        $('#songInfo').empty();
-        $('#songInfo').append(`<img src="spinner.gif" id="spinner" alt="loading"/>`);
-        let queryText = $('#songSearch').val();
-        $('.js-error-message').empty();
-        searchForLyrics(queryText);
-    });
+function handleSearch() {
+  $('#searchArea').on('submit', event => {
+    event.preventDefault();
+    $('#songInfo').empty();
+    $('#songInfo').append(`<img src="./assets/img/spinner.gif" id="spinner" alt="loading"/>`);
+    let queryText = $('#songSearch').val();
+    $('.js-error-message').empty();
+    searchForLyrics(queryText);
+  });
 }
 
-function handleClicks(){
+function handleClicks() {
   handleSearch();
   handleGetLyrics();
   handleGetYoutubeVideo();
